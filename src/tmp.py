8166f6a2738f3a493bb2a443a4bdb8caa479167c
pengfei <liu.pengfei@hotmail.fr>
2021-02-23 15:46:57 +0100

    update

--- src/tmp.py
+++ src/tmp.py
@@ -1,30 +1,4 @@
-import pandas as pd
 import numpy as np
-
-series = [('Stranger Things', 3, 'Millie'),
-          ('Game of Thrones', 8, 'Emilia'),
-          ('La Casa De Papel', 4, 'Sergio'),
-          ('Westworld', 3, 'Evan Rachel'),
-          ('Stranger Things', 3, 'Millie'),
-          ('La Casa De Papel', 4, 'Sergio')]
-
-# Create a DataFrame object
-df = pd.DataFrame(series, columns=['Name', 'Seasons', 'Actor'])
-duplicated_element = df.duplicated()
-
-# Find a duplicate rows
-duplicateDFRow = df[duplicated_element]
-
-print(duplicateDFRow)
-
-df_final = df.drop_duplicates()
-print(df_final.head())
-df_final.to_csv('myfile.csv',index=False)
-
-# duplicated_element_df=pd.DataFrame(duplicated_element,columns=['index','duplicated'])
-# print(duplicated_element_df.head())
-# for col in duplicated_element_df.columns:
-#     print(col)
 
 my_schema = {'index': np.int64,
              'Name': str,
