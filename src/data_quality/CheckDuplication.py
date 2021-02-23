b2cf265d618a3caa5806ae230e98dbec76868020
pengfei <liu.pengfei@hotmail.fr>
2021-02-23 11:47:48 +0100

    update check duplication for output argo pipeline params

--- src/data_quality/CheckDuplication.py
+++ src/data_quality/CheckDuplication.py
@@ -14,14 +14,14 @@ def get_duplicated_row_numbers(duplicate_row_df):
     return len(duplicate_row_df.index)
 
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     print('Number of arguments that you give is wrong, please enter the path of the file which you want to analyze.')
 else:
-    file_path = "/home/pliu/data_set/argo_data_pipeline/pokemon-bad.csv"
-    path = sys.argv[1]
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
     # print(path)
-    df = pd.read_csv(path, index_col=0)
-    result_str = '###################################################\n'\
+    df = pd.read_csv(input_path, index_col=0)
+    result_str = '###################################################\n' \
                  'The total number of duplicated rows is $duplicated_row_number\n' \
                  '###################################################\n' \
                  'The duplicated rows are: \n' \
@@ -29,6 +29,15 @@ else:
                  '###################################################\n'
     temp_obj = Template(result_str)
     duplicated_rows = get_duplicated_rows(df)
-    result = temp_obj.substitute(duplicated_row_number=get_duplicated_row_numbers(duplicated_rows),
+    duplicated_row_numbers = get_duplicated_row_numbers(duplicated_rows)
+    result = temp_obj.substitute(duplicated_row_number=duplicated_row_numbers,
                                  duplicated_rows=duplicated_rows.to_string())
     print(result)
+    f = open(output_path, "w")
+    if duplicated_row_numbers >= 1:
+        f.write("True")
+    else:
+        f.write("False")
+    f.close()
+
+# python CheckDuplication.py /home/pliu/data_set/argo_data_pipeline/pokemon-bad.csv /tmp/output_params.txt
